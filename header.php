073e315fd2fe1c7b7254c98aed6eae7ebd7cbe92
djbeefburger <beefburger@gmail.com>
2024-08-26 20:22:13 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,78 @@
 <?php
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="src/css/images/favicon.ico">
+
+    <title>Bullets and Bandaids as Episodes</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="src/css/main.css" rel="stylesheet">
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <!-- Fixed navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-left" href="#"><img src="logo.png" alt="djbeefburger"></a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li <?php echo (empty($_GET['q'])?'class="active"':""); ?>><a href="./">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="contact.php">Connect</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Listen Now!</a></li>
+                <li><a href="/?q=shop">Shop</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Sort Options</li>
+                <li><a href="#">To the Front</a></li>
+                <li><a href="#">To the Back</a></li>
+				<li><a href="#">Get Wild</a></li>
+              </ul>
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li <?php echo ((empty($_GET['q'])||$_GET['q']=='all')?'class="active"':""); ?>><a href="/?q=all">Show All</a></li>
+            <li <?php echo ((!empty($_GET['q'])&&$_GET['q']=='mix')?'class="active"':""); ?>><a href="/?q=mix">Mixes</a></li>
+			<li <?php echo ((!empty($_GET['q'])&&$_GET['q']=='single')?'class="active"':""); ?>><a href="/?q=single">Singles</a></li>
+            <li <?php echo ((!empty($_GET['q'])&&$_GET['q']=='top')?'class="active"':""); ?>><a href="/?q=top">Top Picks <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
